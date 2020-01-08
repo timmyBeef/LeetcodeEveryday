8d4194d70975d1db9e11d818b6dad143501a06a5
timmyEvil <timmy@cht.com.tw>
2020-01-08 17:46:54 +0800

    prepare for jan interviews

---
+++ src/leetcode/backtracking/CombinationSum2.java
@@ -0,0 +1,33 @@
+package leetcode.backtracking;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CombinationSum2 {
+    public List<List<Integer>> combinationSum2(int[] nums, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(list, new ArrayList<>(), nums, target, 0);
+        return list;
+
+    }
+
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
+        if(remain < 0) return;
+        else if(remain == 0) list.add(new ArrayList<>(tempList));
+        else{
+            for(int i = start; i < nums.length; i++){
+                if(i > start && nums[i] == nums[i-1]) continue; // skip duplicates
+                tempList.add(nums[i]);
+                backtrack(list, tempList, nums, remain - nums[i], i + 1);
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        int nums[] = {1, 2, 3, 5, 4, 6};
+        System.out.println(new CombinationSum2().combinationSum2(nums, 6));
+    }
+}

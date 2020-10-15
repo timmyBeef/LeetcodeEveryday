c755e57b77ee5e1546e4297ba9bc1ba26faae27a
timmy <timmybeeflin@gmail.com>
2020-10-15 15:52:29 +0800

    Bit Manipulation: 136,191,231,389

--- src/leetcode/bit/kotlin/NumberOf1Bits.kt
+++ src/leetcode/bit/kotlin/NumberOf1Bits.kt
@@ -1,2 +1,13 @@
 package leetcode.bit.kt
 
+fun hammingWeight(n:Int):Int {
+    if (n == 0) return 0
+
+    var res = 0
+    var num = n
+    for(i in 0 until 32) {
+        res += num and 1
+        num = num shr 1
+    }
+    return res
+}

d733a291adad5b745ef4976cecaec71fde4561b2
timmy <timmybeeflin@gmail.com>
2020-10-16 22:58:12 +0800

    Bit Manipulation: leetcode 318 add comment

--- src/leetcode/bit/java/MaximumProductOfWordLengths.java
+++ src/leetcode/bit/java/MaximumProductOfWordLengths.java
@@ -1,5 +1,9 @@
 package leetcode.bit.java;
 
+/*
+    time: O(n^2)
+    space: O(n)
+ */
 public class MaximumProductOfWordLengths {
     public int maxProduct(String[] words) {
         int wLen = words.length;
@@ -10,15 +14,15 @@ public class MaximumProductOfWordLengths {
         for (int i = 0; i < wLen; i++) {
             int val = 0;
             for (int j = 0; j < words[i].length(); j++) {
-                val |= (1 << (words[i].charAt(j) - 'a')); // 把每個單詞的字做佔位
+                val |= (1 << (words[i].charAt(j) - 'a')); // 把每個單詞的字做佔位 (or)
             }
             bytes[i] = val; // 存起來
         }
 
         for (int i = 0; i < wLen; i++) {
             for (int j = i + 1; j < wLen; j++) {
-                if ((bytes[i] & bytes[j]) == 0) { // 一個跟一個做 or 比較, 0 代表佔位不重複，也就是完全不同的字母
-                    res = Math.max(res, words[i].length()*words[j].length()); //計算最長長度
+                if ((bytes[i] & bytes[j]) == 0) { // 一個單詞跟一個單詞做 and 比較, 結果 0 代表彼此佔位不重複，也就是彼此是完全不同的字母
+                    res = Math.max(res, words[i].length()*words[j].length()); // 計算最長長度
                 }
             }
         }
